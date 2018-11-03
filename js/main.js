62893b62a58fc9fdd42cd04942f72cb21aff6dbd
hanane <nam.hana@yahoo.fr>
2018-11-03 23:04:32 +0100

    add an avatar and some changes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-<script>
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+if (window.matchMedia("(max-width: 600px)").matches) {
+  var button = document.getElementsByClassName("htPage");
+  var limit = document.body.offsetHeight - window.innerHeight;
+
+
+  window.onscroll = function() {
+    if (document.body.scrollTop > (limit - 20) || document.documentElement.scrollTop > (limit - 20)) {
+      button[0].style.bottom = "50px";
+    } else {
+      button[0].style.bottom = "10px";
+    }
+  };
 }
-</script>
